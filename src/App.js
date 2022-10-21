aba6a2589ebde6366c695788e618594944c0a16a
Utkarsh <utripathi2002@gmail.com>
2022-10-21 23:27:54 +0530

    landing page added by akshat

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Backdrop from "./components/Backdrop/Backdrop";
 import Landing from "./Pages/Landing";
-import { Parallax, ParallaxLayer } from '@react-spring/parallax'
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 import Navbar from "./components/Navbar";
 import Events from "./components/Events/Events";
 import Sponsors from "./components/Sponsors/Sponsors";
@@ -8,23 +8,19 @@ import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
-    (
-    (
     <div className="App">
-      <Parallax pages={2} style={{top: '0', left: '0'}}>
+      <Parallax pages={2} style={{ top: "0", left: "0" }}>
         <ParallaxLayer offset={0} speed={17}>
           <Landing />
         </ParallaxLayer>
-        <Backdrop>
-          <Events />
-          <Sponsors />
-          <Footer />
-        </Backdrop>
       </Parallax>
+      <Backdrop>
+        <Events />
+        <Sponsors />
+        <Footer />
+      </Backdrop>
       <Navbar />
     </div>
-  )
-  )
   );
 }
 
