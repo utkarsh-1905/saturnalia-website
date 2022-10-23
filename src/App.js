8e294cd51d917690e9bdbb1ec0cb7dc2c9670eba
destrex271 <destrex271@gmail.com>
2022-10-23 22:24:25 +0530

    Extra Footer Fixed: Parallax removed

--- src/App.js
+++ src/App.js
@@ -9,17 +9,12 @@ import Footer from "./components/Footer/Footer";
 function App() {
   return (
     <div className="App">
-      <Parallax pages={2} style={{ top: "0", left: "0" }}>
-        <ParallaxLayer offset={0} speed={2}>
-          {/* <Landing /> */}
-          <Backdrop>
-            <Landing />
-            <Events />
-            <Sponsors />
-            <Footer />
-          </Backdrop>
-        </ParallaxLayer>
-      </Parallax>
+      <Backdrop>
+        <Landing />
+        <Events />
+        <Sponsors />
+        <Footer />
+      </Backdrop>
       <Navbar />
     </div>
   );
