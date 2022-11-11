c9d4a1823c48aebe2f7c1c186f6b36c10b63377d
Utkarsh <utripathi2002@gmail.com>
2022-11-11 14:44:36 +0530

    changes in event card

--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -1,27 +1,33 @@
 import React from "react";
 import styles from "./events.module.scss";
 import { Swiper, SwiperSlide } from "swiper/react";
-// import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+import { Autoplay, Keyboard } from "swiper";
 import "swiper/css";
-import EventCard from '../../components/events-card/EventCard';
+import "swiper/css/autoplay";
+import EventCard from "../../components/events-card/EventCard";
 
 const Events = () => {
   return (
     <div className={styles.container}>
       <h1 className={styles.eventHeading}>EVENTS</h1>
-      <div className={styles.groundmesh}></div>
+      {/* <div className={styles.groundmesh}></div> */}
       <div className={styles.swiperContainer}>
-        <Swiper spaceBetween={50} slidesPerView={3}>
-          <SwiperSlide>
+        <Swiper
+          spaceBetween={50}
+          slidesPerView={3}
+          modules={[Autoplay]}
+          autoplay={true}
+        >
+          <SwiperSlide className={styles.swiperCard}>
             <EventCard />
           </SwiperSlide>
-          <SwiperSlide>
+          <SwiperSlide className={styles.swiperCard}>
             <EventCard />
           </SwiperSlide>
-          <SwiperSlide>
+          <SwiperSlide className={styles.swiperCard}>
             <EventCard />
           </SwiperSlide>
-          <SwiperSlide>
+          <SwiperSlide className={styles.swiperCard}>
             <EventCard />
           </SwiperSlide>
         </Swiper>
