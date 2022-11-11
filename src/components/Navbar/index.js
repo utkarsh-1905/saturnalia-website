c189883ff2d1825a1de94f687af01cb34abea9d5
Utkarsh <utripathi2002@gmail.com>
2022-11-11 19:05:39 +0530

    testing form

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,29 +1,48 @@
-import React from 'react'
-import navStyles from './navstyles.module.css'
-
+import React from "react";
+import navStyles from "./navstyles.module.css";
+import NonThapar from "../Forms/NonThapar";
 export default function Navbar() {
   return (
     <div className={navStyles.footer}>
-        {/* Navbar */}
-        <div className={[navStyles.navbar]}>
-            <div className={navStyles.navLeft}>
-                <a className={navStyles.navlink} href="">Home</a>
-                <a className={navStyles.navlink} href="">Events</a>
-                <a className={navStyles.navlink} href="">Features</a>
-            </div>
-            <div className={navStyles.buttonTray}>
-                <button
-                    className={navStyles.loginButton}
-                >
-                    <em>LOG IN</em>
-                </button>
-            </div>
-            <div className={navStyles.navRight}>
-                <a className={navStyles.navlink} href="">Sponsors</a>
-                <a className={navStyles.navlink} href="">FAQS</a>
-                <a className={navStyles.navlink} href="">Contact Us</a>
-            </div>
+      <div className={[navStyles.navbar]}>
+        <div className={navStyles.navLeft}>
+          <a className={navStyles.navlink} href="">
+            Home
+          </a>
+          <a className={navStyles.navlink} href="">
+            Events
+          </a>
+          <a className={navStyles.navlink} href="">
+            Features
+          </a>
         </div>
+        <div className={navStyles.buttonTray}>
+          <button className={navStyles.loginButton}>
+            <em>LOG IN</em>
+          </button>
+        </div>
+        <div className={navStyles.navRight}>
+          <a className={navStyles.navlink} href="">
+            Sponsors
+          </a>
+          <a className={navStyles.navlink} href="">
+            FAQS
+          </a>
+          <a className={navStyles.navlink} href="">
+            Contact Us
+          </a>
+        </div>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <NonThapar />
+      </div>
     </div>
-  )
+  );
 }
