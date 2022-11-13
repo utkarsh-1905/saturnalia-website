69f0f9ca6d8a6b38e22e1782557a58dab1456bb8
Utkarsh <utripathi2002@gmail.com>
2022-11-13 14:51:43 +0530

    email verification page integrated

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,20 +5,20 @@ import Facebook from "../../assets/facebook.svg";
 import Instagram from "../../assets/instagram.svg";
 import Twitter from "../../assets/twitter.svg";
 import Youtube from "../../assets/youtube.svg";
-import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+// import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 import EmailIcon from "@mui/icons-material/Email";
 import CallIcon from "@mui/icons-material/Call";
-import { motion } from "framer-motion";
+// import { motion } from "framer-motion";
 
 const Footer = () => {
   return (
     <div className={styles.container}>
       <div className={styles.left}>
         <img src={Logo} alt="Saturnalia 2022" />
-        <p className={styles.footerInfo}>
+        {/* <p className={styles.footerInfo}>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
           vulputate libero et velit interdum, ac aliquet odio mattis.
-        </p>
+        </p> */}
         <div className={styles.logos}>
           <img src={Facebook} alt="Facebook" />
           <img src={Instagram} alt="Instagram" />
@@ -47,13 +47,13 @@ const Footer = () => {
             <CallIcon className={styles.contactIcons} /> 123456789
           </p>
         </div>
-        <motion.button
+        {/* <motion.button
           className={styles.toTop}
           initial={{ y: 0 }}
           whileHover={{ y: -10 }}
         >
           <KeyboardArrowUpIcon fontSize="large" />
-        </motion.button>
+        </motion.button> */}
       </div>
     </div>
   );
--- src/components/email-verified/index.js
+++ src/components/email-verified/index.js
@@ -1,13 +1,14 @@
-import './index.css';
-import Email from './Email';
-import Backdrop from '../Backdrop/Backdrop'
-function App() {
+import "./index.css";
+import Email from "./Email";
+import Backdrop from "../Backdrop/Backdrop";
+
+function EmailVerified() {
   return (
     <div className="App">
-      <Backdrop/>
-      <Email/>
+      <Backdrop />
+      <Email />
     </div>
   );
 }
 
-export default App;
+export default EmailVerified;
