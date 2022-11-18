479450e8d0d72bc5ee238a1cbe3612bfcca6ce69
simardeepsm <smudhar_be21@thapar.edu>
2022-11-18 22:12:35 +0530

    new-highlights-template

--- src/components/Highlights/Highlights.js
+++ src/components/Highlights/Highlights.js
@@ -1,36 +1,34 @@
 import React, { useEffect,useState } from 'react'
-import'./highlights.css'
+import img1 from './images/back.jpeg'
+import SplitImage from './SplitImage'
+
+import'./newhighlights.css'
 
 const Highlights = () => {
   
-  const images=[
-    {
-      img1:"./highlights/back.jpeg"
-    }
-  ]
-  // useEffect(()=>{
-  // console.log(window.innerWidth,window.innerHeight);
-  // },[])
+  // const images=[
+  //   {
+  //     img1:"./highlights/back.jpeg"
+  //   }
+  // ]
+  useEffect(()=>{
+  console.log(window.innerWidth,window.innerHeight);
+  },[])
 
   return (
     <div className='container'>    
-    <div className='highlights-horizontal-flex'>
-    <div className='left-div'>
-      <div className='left-div-content'>
-        <div className='left-div-content-img'>
-
-        </div>
-        </div>
-        
+   
+    <div className='left-highlight'>
+    {/* <img className='image-highlight' src={img1}/> */}
+    <SplitImage/>
     </div>
-    <div className='right-div'>
-      <div className='right-div-content'>
-    <h1 className='eventHeading'>HIGHLIGHTS</h1>
+
+
+    <div className='right-highlight'>
+    <h1 className='eventHead'>HIGHLIGHTS</h1>
     <p className='highlight-content'>This years' guests include some of the most popular artists from across the country. This collaboration is super exciting for all of us as we prepare to deliver some of the most phenomenal performances that Saturnalia has ever experienced.</p>
-</div>
-
     </div>
-    </div>
+   
     </div>
   )
 }
