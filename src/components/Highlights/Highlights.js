ee66245048fff6f56067ee4d57df099f63431a71
Vishist Bhagabati <55060840+VishistB@users.noreply.github.com>
2022-11-23 01:06:58 +0530

    replaced astronaut with zaeden img

--- src/components/Highlights/Highlights.js
+++ src/components/Highlights/Highlights.js
@@ -28,11 +28,14 @@ const Highlights = () => {
 
     <div className='right-highlight'>
     <h1 className='eventHead'>HIGHLIGHTS</h1>
-    <p className='highlight-content'>This years' guests include some of the most popular artists from across the country. This collaboration is super exciting for all of us as we prepare to deliver some of the most phenomenal performances that Saturnalia has ever experienced.</p>
+    {/* <p className='highlight-content'>This years' guests include some of the most popular artists from across the country. This collaboration is super exciting for all of us as we prepare to deliver some of the most phenomenal performances that Saturnalia has ever experienced.</p> */}
+    <p className='highlight-content-artists'>Get ready to have the experience of a lifetime as Christmas comes a tad bit early this year!
+      Yes, you heard that right! Saturnalia is ecstatic to announce that the headliner for this year is none other than,*drumroll* Zaeden, The man behind ‘Tere Bina'- the song that broke the charts with over 20 million views on YouTube!
+      Experience an unparalleled catalog of melodies that are sure to make you sing your heart out from one of the most charming artists in the industry. This will be a concert that none would like to miss!</p>
     </div>
 
     </div>
-    <div className='artists'>
+    {/* <div className='artists'>
       <div>
         <img className='zaeden' src={zaeden}></img>
         <h1 className='eventHead'>Zaeden</h1>
@@ -42,7 +45,7 @@ const Highlights = () => {
       Yes, you heard that right! Saturnalia is ecstatic to announce that the headliner for this year is none other than,*drumroll* Zaeden, The man behind ‘Tere Bina'- the song that broke the charts with over 20 million views on YouTube!
       Experience an unparalleled catalog of melodies that are sure to make you sing your heart out from one of the most charming artists in the industry. This will be a concert that none would like to miss!</p>
       </div>
-    </div>
+    </div> */}
 
     </div>
     
