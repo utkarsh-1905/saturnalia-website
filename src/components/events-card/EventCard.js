20f5c3f0109b8b5e901528185b3e382989b57a29
destrex271 <destrex271@gmail.com>
2022-11-23 20:07:51 +0530

    redirect bug fixed

--- src/components/events-card/EventCard.js
+++ src/components/events-card/EventCard.js
@@ -18,11 +18,18 @@ const EventCard = ({ event }) => {
   // }
 
   const registerTeam = () => {
+    if(event.redirect_link != null){
+      window.open(event.redirect_link);
+      return;
+    }
     showTeamModal(true);
   }
   const register = () => {
     console.log(cookie.authToken);
-
+    if(event.redirect_link != null){
+      window.open(event.redirect_link);
+      return;
+    }
     if (cookie.authToken) {
       console.log(event.id);
       var data = new FormData();
@@ -90,25 +97,16 @@ const EventCard = ({ event }) => {
           <div className="event-card-button-div">
             {event.registration_allowed && (
               !event.is_team_event?
-                ( event.name === 'Smart Punjab Hackathon' || 'Place-O-Pedia' ?
-                (<button className="card-event-register" onClick={()=>{window.open(`${event.redirect_link}`,'_blank')}}>
-                  Register Now
-                </button>) 
-                :
-                (<button className="card-event-register" onClick={register}>
-                Register Now
-              </button>)
-
+                (
+                  <button className="card-event-register" onClick={register}>
+                    Register Now
+                  </button>
                 )
                 :
-                ( event.name === 'Smart Punjab Hackathon' || 'Place-O-Pedia' ? 
-                (<button className="card-event-register" onClick={()=>{window.open(`${event.redirect_link}`,'_blank')}}>
-                Register/Join Team
-              </button>):(
+                (
                   <button className="card-event-register" onClick={registerTeam}>
                     Register/Join Team
                   </button>
-                )
                 )
             )}
           </div>
